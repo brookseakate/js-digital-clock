5d5f43c30dfafcd50adb6137c0b5e17afa978c31
Kate Shaffer <kate.shaffer@gmail.com>
2016-11-18 15:12:36 -0800

    OMG it's working

--- index.js
+++ index.js
@@ -1,33 +1,10 @@
-// Your code here
 var showTime = function() {
   this.time = new Date();
   return this.time.toLocaleTimeString();
 };
 
-
-// var showTime = function() {
-//   this.time = new Date();
-//   this.hour = Hour(this.time);
-//   this.minute = Minute(this.time);
-//   this.second = Second(this.time);
-//   return this.hour + ":" + this.minute + ":" + this.second;
-// };
-//
-// var Hour = function(time) {
-//   return time.getUTCHours();
-// };
-//
-// var Minute = function(time) {
-//   return time.getUTCMinutes();
-// };
-//
-// var Second = function(time) {
-//   return time.getUTCSeconds();
-// };
-//
-
-
-
 $(document).ready(function() {
-  $('#clock').append(showTime);
+  var intervalID = window.setInterval( function() {
+    $('#clock').html(showTime);
+  }, 1000);
 });
