fabf1c9f5b0aa868c2f1ab5891f89e016a8837c1
Kate Shaffer <kate.shaffer@gmail.com>
2016-11-18 14:56:12 -0800

    Initial commit; UTC hours, minutes, seconds display. Bad formatting.

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 // Your code here
+var showTime = function() {
+  this.time = new Date();
+  this.hour = Hour(this.time);
+  this.minute = Minute(this.time);
+  this.second = Second(this.time);
+  return this.hour + ":" + this.minute + ":" + this.second;
+};
+
+var Hour = function(time) {
+  return time.getUTCHours();
+};
+
+var Minute = function(time) {
+  return time.getUTCMinutes();
+};
+
+var Second = function(time) {
+  return time.getUTCSeconds();
+};
+
+
+
+
+$(document).ready(function() {
+  $('#clock').append(showTime);
+});
